87cf8bb052dcc6913c58288c3312f83fe335ffc2
Droid-An <andrei.filippov.aaf@gmail.com>
2025-06-19 18:47:34 +0100

    done ical file generation

--- generate-ical.mjs
+++ generate-ical.mjs
@@ -1,9 +1,32 @@
-
-
-// This is a placeholder file which shows how you can access functions and data defined in other files. You can delete the contents of the file once you have understood how it works.
-// It can be run with `node`.
-
-import { getGreeting } from "./common.mjs";
-import daysData from "./days.json" with { type: "json" };
-
-console.log(`{getGreeting()} - there are ${daysData.length} known days`);
+import { calculateSpecialDay } from './utils/common.mjs';
+import daysData from './data/days.json' with { type: 'json' };
+import fs from 'fs';
+function generateICS() {
+  let ics = [
+    'BEGIN:VCALENDAR',
+    'VERSION:2.0',
+    'CALSCALE:GREGORIAN'
+  ];
+  for (let year = 2020; year <= 2030; year++) {
+    for (const { monthName, name, descriptionURL, ...dayInfo } of daysData) {
+      const month = new Date(`${monthName} 1, ${year}`).getMonth();
+      const day = calculateSpecialDay(year, month, dayInfo);
+      if (!day) continue;
+      const dateStr = new Date(year, month, day)
+        .toISOString()
+        .slice(0, 10)
+        .replace(/-/g, '');
+      ics.push(
+        'BEGIN:VEVENT',
+        `SUMMARY:${name}`,
+        `DTSTART;VALUE=DATE:${dateStr}`,
+        `DTEND;VALUE=DATE:${dateStr}`,
+        `DESCRIPTION:See ${descriptionURL}`,
+        'END:VEVENT'
+      );
+    }
+  }
+  ics.push('END:VCALENDAR');
+  fs.writeFileSync('days.ics', ics.join('\n'));
+}
+generateICS();
