291feefc433eb5061dbd39eb391adfa8c67d50a8
Droid-An <andrei.filippov.aaf@gmail.com>
2025-06-19 21:20:12 +0100

    fixed ics file generation

--- generate-ical.mjs
+++ generate-ical.mjs
@@ -1,32 +1,29 @@
-import { calculateSpecialDay } from './utils/common.mjs';
+import { calculateSpecialDay } from "./utils/common.mjs";
 import daysData from './data/days.json' with { type: 'json' };
-import fs from 'fs';
+import fs from "fs";
 function generateICS() {
-  let ics = [
-    'BEGIN:VCALENDAR',
-    'VERSION:2.0',
-    'CALSCALE:GREGORIAN'
-  ];
+  let ics = ["BEGIN:VCALENDAR", "VERSION:2.0", "CALSCALE:GREGORIAN"];
   for (let year = 2020; year <= 2030; year++) {
     for (const { monthName, name, descriptionURL, ...dayInfo } of daysData) {
       const month = new Date(`${monthName} 1, ${year}`).getMonth();
       const day = calculateSpecialDay(year, month, dayInfo);
       if (!day) continue;
-      const dateStr = new Date(year, month, day)
-        .toISOString()
-        .slice(0, 10)
-        .replace(/-/g, '');
+      const pad = (n) => String(n).padStart(2, "0");
+      const dateObj = new Date(year, month, day);
+      const dateStr = `${dateObj.getFullYear()}${pad(
+        dateObj.getMonth() + 1
+      )}${pad(dateObj.getDate())}`;
       ics.push(
-        'BEGIN:VEVENT',
+        "BEGIN:VEVENT",
         `SUMMARY:${name}`,
         `DTSTART;VALUE=DATE:${dateStr}`,
         `DTEND;VALUE=DATE:${dateStr}`,
         `DESCRIPTION:See ${descriptionURL}`,
-        'END:VEVENT'
+        "END:VEVENT"
       );
     }
   }
-  ics.push('END:VCALENDAR');
-  fs.writeFileSync('days.ics', ics.join('\n'));
+  ics.push("END:VCALENDAR");
+  fs.writeFileSync("days.ics", ics.join("\n"));
 }
 generateICS();
